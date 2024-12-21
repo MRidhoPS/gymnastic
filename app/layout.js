39cb7bd1625d21019c89f539aa4b61db47bf907d
MRidhoPS <mridhophs@gmail.com>
2024-12-21 10:02:39 +0700

    fix sitemap, robots and layout.js

--- app/layout.js
+++ app/layout.js
@@ -45,6 +45,7 @@ export default function RootLayout({ children }) {
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content={metadata.author} />
         <meta property="og:title" content={metadata.openGraph.title} />
+        <meta name="google-site-verification" content="googlec0ca832322086f5b.html" />
         <meta property="og:description" content={metadata.openGraph.description} />
         <meta property="og:type" content={metadata.type} />
         <meta property="og:url" content="https://gymnastic-a65e.vercel.app/" />
